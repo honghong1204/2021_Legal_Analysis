315712cd9a07897621100f91f4593c51db9a7e21
honghong1204 <78341661+honghong1204@users.noreply.github.com>
2021-06-03 05:51:01 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,53 +1,54 @@
-from bs4 import BeautifulSoup
-import requests
-# import re
+import cn2an
 
-results = []
-original_website_of_results = []
-# counter = -1
-# code_to_change = input("Input code_to_change:")
-original_url = input("請輸入第一個裁判書的連結:")
+cn_digit = {'一': 1, '二': 2, '三': 3, '四': 4, '五': 5, '六': 6, '七': 7, '八': 8, '九': 9,
+            '十': 10, '百': 100, '千': 1000, "萬": 10000}
+trad_num = {'壹': 1, '貳': 2, '參': 3, '四': 4, '五': 5, '六': 6, '七': 7, '八': 8, '九': 9,
+            '十': 10, '百': 100, '千': 1000, "萬": 10000}
+cn_digit_pure = [, '一', '二', '三', '四', '五', '六', '七', '八', '九']
+arab_digit_pure = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 
-
-def url_spliting(original_url_):
-    a = original_url_.split("q=")
-    b = a[1].split("&sort")
-    return b[0]
+def trad_num_to_simp_num(to_change):
 
 
-def def_url(counter_, code_to_change_):
-    url_front = "https://law.judicial.gov.tw/FJUD/data.aspx?ro="
-    url_mid = "&q="
-    url_last = "&sort=DS&ot=in"
-    return url_front + str(counter_) + url_mid + code_to_change_ + url_last
+
+def list_to_string(inputed_list):
+    # inputed_list = trad_num_to_simp_num(inputed_list)
+    return "".join(inputed_list)
 
 
-def html_retrieving():
-    url = def_url(counter, url_spliting(original_url))
-    html = requests.get(url)
-    html.encoding = "UTF-8"
-    bSoup = BeautifulSoup(html.text, 'html5lib')
-    to_decode = bSoup.find_all(class_="htmlcontent")
-    if (len(to_decode)) == 0:
-        print("Check Your Address!")
-        exit(0)
-    if to_decode[0].text == "":
-        to_decode = bSoup.find_all(class_="text-pre text-pre-in")
-    original_website_of_results.append(def_url(counter, url_spliting(original_url)))
-    results.append(to_decode[0].text)
+def chn_to_arabic(string_to_translate):
+    return cn2an.cn2an(string_to_translate, "smart")
 
 
-def get_url(num: int):
-    print(def_url(num - 1, url_spliting(original_url)))
-    exit(0)
+inputed_string = "被告持有新臺幣三千元整及新臺幣2千元整"
+listed_inputed_string = list(inputed_string)
+record_i = 0
+new_list = []
+i = 0
+z = 0
+while z == 0:
+    i += 1
+    j = 0
+    # print(listed_inputed_string, "TOP")
+    if listed_inputed_string[i] == "元":
+        while cn_digit.__contains__(listed_inputed_string[i - 1]) or arab_digit_pure.__contains__(
+                listed_inputed_string[i - 1]) or trad_num.__contains__(listed_inputed_string[i - 1]):
+            if j == 0:
+                record_i = i
+            j = 1
+            i = i - 1
+        for k in range(i, record_i, 1):
+            # print("k=", k, "recordI =", record_i)
+            new_list.append(listed_inputed_string[k])
+            # print(new_list, "A")
+        new_string = list_to_string(new_list)
+        del listed_inputed_string[i:record_i]
+        listed_inputed_string.insert(i, str(chn_to_arabic(new_string)))  # 本行置入要代換的字元，即中文換數字的部分
+        i = i + 1
+        new_list = []
 
-
-# 若要查看特定判決對應的原始URL，將下列的#號移除，並將括號中的數字改為目標判決所標示之號碼
-# get_url(eval(input("請輸入判決主文上方的數字:")))
-num_to_search = eval(input("請輸入要查詢的資料數:"))
-for counter in range(0, num_to_search, 1):
-    # counter = counter + 1
-    print(counter + 1)
-    html_retrieving()
-    print(results[counter])
-    print("\n")
+    # print("i=", i, "len(listed)=", len(listed_inputed_string))
+    if i == len(listed_inputed_string) - 1:
+        break
+final_string = list_to_string(listed_inputed_string)
+print(final_string)
